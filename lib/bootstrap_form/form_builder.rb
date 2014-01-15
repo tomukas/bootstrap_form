8587bf786f9d546866d6ddad552502b497bccc16
Tomas Aleksiejunas <taleksiejunas@gmail.com>
2014-01-15 11:43:18 +0200

    Update form_builder.rb

--- lib/bootstrap_form/form_builder.rb
+++ lib/bootstrap_form/form_builder.rb
@@ -28,7 +28,7 @@ module BootstrapForm
             
             elsif method_name == 'search_field'
             
-              content_tag(:div, super(name, *args) + content_tag(:button, content_tag(:i, nil, class:'icon-inverse icon-white', id:'search_append_button'), class: 'btn btn-warning'), class: 'input-append') + help
+              content_tag(:div, super(name, *args) + content_tag(:button, content_tag(:i, nil, class:'icon-search icon-white', id:'search_append_button'), class: 'btn btn-inverse'), class: 'input-append') + help
            
             else
              super(name, *args) + help
